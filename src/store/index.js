3235f71c4800d9fd1ca3a4d61360d07e04cd0503
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2024-03-07 19:07:48 +0100

    store implemented

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore } from 'vuex'
+import * as getters from './getters'
+import * as actions from './actions'
+import mutations from './mutations'
+
+const state = {
+  user: {
+    name: null
+  },
+  teams: [/* {id, name} */],
+  boards: [/* {id, name, description, teamId} */]
+}
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  actions,
+  mutations
 })
