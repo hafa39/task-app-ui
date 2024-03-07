ab3afa49001786d2580726b0677e36c04def9c5f
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2024-03-07 18:25:40 +0100

    board cart component added

---
+++ src/components/BoardCart.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mr-5" outlined>
+    <v-card-title> {{board.name}} </v-card-title>
+    <v-card-subtitle> {{ board.description }}</v-card-subtitle>
+    <v-card-text> text</v-card-text>
+    <v-card-actions >
+      <v-btn rounded color="" right v-on:click="openBoard">Edit</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "BoardCart",
+  props : {
+    board : {
+      required : false,
+      type: Object
+    }
+  },
+  methods :{
+    openBoard(){
+      this.$emit("openBoard",this.board)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
